b00f96fc7cee461014127dd7af8515d7ce06abad
Narmin <nermin.huseynzade002@gmail.com>
2023-10-19 16:45:34 +0400

    commit

--- script.js
+++ script.js
@@ -5,3 +5,51 @@ sliderEl.addEventListener("input", (event) => {
     
     sliderEl.style.background = `linear-gradient(to right, #3563E9 ${progress}%, #ccc ${progress}%)`;
 })
+
+let filter = document.getElementById("filter")
+let side = document.getElementById("side")
+let type = document.getElementById("type")
+let capacity = document.getElementById("capacity")
+let topa = document.getElementById("top")
+let items = document.getElementsByClassName("items")
+let open = false
+
+filter.onclick = function() {
+    if(!open) {
+        side.style.display = "block";
+        type.style.display = "none";
+        capacity.style.display = "none"
+        items[0].style.gridTemplateColumns = "auto auto auto"
+        topa.style.display = "none"
+        open = true
+    }
+
+    else {
+        side.style.display = "none";
+        type.style.display = "block";
+        capacity.style.display = "block"
+        items[0].style.gridTemplateColumns = "auto auto auto auto"
+        topa.style.display = "flex"
+        open = false
+    }
+}
+
+let heart = document.getElementsByClassName("heart");
+let likes = []
+
+for(let i = 0; i < heart.length; i++) {
+    likes[i] = false
+}
+
+for(let i = 0; i < heart.length; i++) {
+    heart[i].onclick = function() {
+        if(!likes[i]) {
+            heart[i].src = "like_fill.svg"
+            likes[i] = true
+        }
+        else {
+            heart[i].src = "like_empty.svg"
+            likes[i] = false
+        }
+    }
+}
